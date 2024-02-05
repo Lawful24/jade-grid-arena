ebee388311f2f7e3727f252352fc04c1d9b3eba5
Lawful <tarkanyilacko@gmail.com>
2024-02-05 18:22:35 +0000

    Add properties file support

--- src/main/java/com/napier/Main.java
+++ src/main/java/com/napier/Main.java
@@ -1,7 +1,21 @@
 package com.napier;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Retrieve user parameters from the config file.
+        Properties properties = new Properties();
+
+        try (InputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(properties.getProperty("example"));
     }
 }
