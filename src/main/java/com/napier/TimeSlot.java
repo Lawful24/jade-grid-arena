8a8abe7b79d4d425e85bb9076d184025061b7b68
Lawful <tarkanyilacko@gmail.com>
2024-03-01 09:50:04 +0000

    Add overriding toString and equals methods to TimeSlot

--- src/main/java/com/napier/TimeSlot.java
+++ src/main/java/com/napier/TimeSlot.java
@@ -11,4 +11,22 @@ public class TimeSlot {
     public int getStartHour() {
         return startHour;
     }
+
+    @Override
+    public String toString() {
+        return Integer.toString(this.startHour);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        } else if (obj == this) {
+            return true;
+        } else {
+            TimeSlot timeSlot = (TimeSlot) obj;
+
+            return this.startHour == timeSlot.getStartHour();
+        }
+    }
 }
