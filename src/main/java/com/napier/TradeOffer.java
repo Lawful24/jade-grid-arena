bbe269b349b86c67d9761a3937a2cd5735d8b5e2
Lawful <tarkanyilacko@gmail.com>
2024-03-12 12:42:31 +0000

    Refactor TradeOffer back to a record

--- src/main/java/com/napier/TradeOffer.java
+++ src/main/java/com/napier/TradeOffer.java
@@ -5,35 +5,12 @@ import jade.core.AID;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class TradeOffer implements Serializable {
-    private AID senderAgent;
-    private AID receiverAgent;
-    private TimeSlot timeSlotOffered;
-    private TimeSlot timeSlotRequested;
-
-    public TradeOffer(AID senderAgent, AID receiverAgent, TimeSlot timeSlotOffered, TimeSlot timeSlotRequested) {
-        this.senderAgent = senderAgent;
-        this.receiverAgent = receiverAgent;
-        this.timeSlotOffered = timeSlotOffered;
-        this.timeSlotRequested = timeSlotRequested;
-    }
-
-    public AID getSenderAgent() {
-        return senderAgent;
-    }
-
-    public AID getReceiverAgent() {
-        return receiverAgent;
-    }
-
-    public TimeSlot getTimeSlotOffered() {
-        return timeSlotOffered;
-    }
-
-    public TimeSlot getTimeSlotRequested() {
-        return timeSlotRequested;
-    }
-
+public record TradeOffer(
+        AID senderAgent,
+        AID receiverAgent,
+        TimeSlot timeSlotOffered,
+        TimeSlot timeSlotRequested
+) implements Serializable {
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
@@ -41,6 +18,7 @@ public class TradeOffer implements Serializable {
         if (object == null || getClass() != object.getClass()) return false;
 
         TradeOffer that = (TradeOffer) object;
+
         return Objects.equals(senderAgent, that.senderAgent)
                 && Objects.equals(receiverAgent, that.receiverAgent)
                 && Objects.equals(timeSlotOffered, that.timeSlotOffered)
