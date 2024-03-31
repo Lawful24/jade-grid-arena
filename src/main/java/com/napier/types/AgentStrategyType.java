074a77aa71d004acc850b5e753c0c047e03002d9
Lawful <tarkanyilacko@gmail.com>
2024-03-31 17:09:04 +0100

    Add javadoc comments to the enums

--- src/main/java/com/napier/types/AgentStrategyType.java
+++ src/main/java/com/napier/types/AgentStrategyType.java
@@ -1,5 +1,10 @@
 package com.napier.types;
 
+/**
+ * Contains the strategies that a Household agent can use in the simulation.
+ *
+ * @author László Tárkányi
+ */
 public enum AgentStrategyType {
     SELFISH,
     SOCIAL
--- src/main/java/com/napier/types/ExchangeType.java
+++ src/main/java/com/napier/types/ExchangeType.java
@@ -1,5 +1,10 @@
 package com.napier.types;
 
+/**
+ * Contains the exchange methods that can be used in the simulation.
+ *
+ * @author László Tárkányi
+ */
 public enum ExchangeType {
     MessagePassing,
     SmartContract
